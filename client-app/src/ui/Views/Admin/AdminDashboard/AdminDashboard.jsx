95674cbf3fc0064bf3208b162d5c00d019239331
Ammon Carson <pianoace1315@gmail.com>
2019-08-20 16:42:26 -0600

    added admin dashboard page

--- client-app/src/ui/Views/Admin/AdminDashboard/AdminDashboard.jsx
+++ client-app/src/ui/Views/Admin/AdminDashboard/AdminDashboard.jsx
@@ -4,8 +4,25 @@ function AdminDashboard() {
     
     return(
         <main>
-            <h1 style={{color: "red"}}>WELCOME TO THE ADMIN DASHBOARD</h1>
-            <p>A Magical Place Where You Can CRUD ALL DAY LONG!!!!</p>
+            <div className="admin__container">
+                <div className="admin__heading-container">
+                    <h1 className="admin__heading">ADMIN DASHBOARD</h1>
+                </div>
+                <div className="admin__option-container">
+                    <div className="admin__section-option-container">
+                        <h2 className="admin__section-heading">Manage Users</h2>
+                        <a className="admin__option" href="/">Add User</a>
+                        <a className="admin__option" href="/">View Users</a>
+                    </div>
+                    <div className="admin__section-option-container">
+                        <h2 className="admin__section-heading">Manage Products</h2>
+                        <a className="admin__option" href="/">Add Product</a>
+                        <a className="admin__option" href="/">View Products</a>
+                        <a className="admin__option" href="/">Add Material</a>
+                        <a className="admin__option" href="/">Add Category</a>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 } 
